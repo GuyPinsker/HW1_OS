4fd77c1e699455d159757ace53f0df095066a554
GuyPinsker <62984079+GuyPinsker@users.noreply.github.com>
2021-05-03 22:54:40 +0300

    added kill, jobs, and updated older funcs 3/5/21

--- commands.cpp
+++ commands.cpp
@@ -6,12 +6,54 @@
 #include <fcntl.h>
 #include <linux/limits.h>
 //********************************************
-// Name Space and local Constatns
+
+// Name Spaces 
 using namespace std;
-char* last_cwd = NULL;
+
+// Local Constants 
+#define NUL '\0'
+enum {MAX_HIST_NUM = 50, CHUNK = 100};
+
+// Local classes and Functions
+class job
+{
+	public:
+		int ser_num;
+		char cmd[MAX_LINE_SIZE];
+		int pid;
+		int entry_time;
+		bool stopped;
+
+		job()
+		{
+			ser_num = 0;
+			cmd[0] = NUL;
+			pid = 0;
+			entry_time = 0;
+			stopped = false;
+		}
+};
+
+int get_job_pid(list<job> jobs, int id)
+{
+	for (auto& iter : jobs)
+	{
+		if (iter.ser_num == id)
+		{
+			return iter.pid;
+		}
+	}
+
+	return -1;
+}
+
+
+
+// Local Variables
+char last_cwd[PATH_MAX] = { NUL };
 char new_last_cwd[PATH_MAX];
 list<char*> hist;
-enum {MAX_HIST_NUM = 50, CHUNK = 100};
+list<job> jobs;
 
 //********************************************
 // function name: ExeCmd
@@ -70,10 +112,10 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 			{
 				if (!strcmp(args[1], "-")) // if ask to go to last cwd
 				{
-					if (last_cwd != NULL) // if this is not the first request (we've been to another place)
+					if (last_cwd[0] != NUL) // if this is not the first request (we've been to another place)
 					{
 						chdir(last_cwd);							 // change to last location
-						last_cwd = new_last_cwd;					 // update last cwd
+						strcpy(last_cwd, new_last_cwd);				 // update last cwd
 					}
 				}
 
@@ -86,13 +128,12 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 
 					else
 					{
-						last_cwd = new_last_cwd;					 // update last cwd
+						strcpy(last_cwd, new_last_cwd);				 // update last cwd
 					}
 				}
 			}
 		}
-	} 
-	
+	} 	
 	/*************************************************/
 	else if (!strcmp(cmd, "pwd")) 
 	{
@@ -111,11 +152,26 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 				cout << wd << endl;
 		}
 	}
-	
 	/*************************************************/
 	else if (!strcmp(cmd, "jobs")) 
 	{
- 		
+		if (num_arg > 1)
+		{
+			illegal_cmd = TRUE;
+		}
+		
+		for (auto& iter : jobs)
+		{
+			int current_time = time(NULL);
+			if (iter.stopped)
+			{
+				cout << "[" << iter.ser_num << "] " << iter.cmd << " : " << iter.pid << " " << current_time - iter.entry_time << " secs (stopped)" << endl;
+			}
+			else
+			{
+				cout << "[" << iter.ser_num << "] " << iter.cmd << " : " << iter.pid << " " << current_time - iter.entry_time << " secs" << endl;
+			}
+		}
 	}
 	/*************************************************/
 	else if (!strcmp(cmd, "showpid")) 
@@ -130,7 +186,21 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 	/*************************************************/
 	else if (!strcmp(cmd, "fg")) 
 	{
+		if (num_arg = 1)
+		{
+			int pid = jobs.back().pid;
+			kill(pid, SIGCONT)
+		}
 		
+		else if (num_arg = 2)
+		{
+			int pid = get_job_pid(jobs, args[1]);
+		}
+
+		else
+		{
+			illegal_cmd = TRUE;
+		}
 	} 
 	/*************************************************/
 	else if (!strcmp(cmd, "bg")) 
@@ -161,13 +231,12 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 		}
 
 		else
-		{// should we add cwd?
+		{
 			int fd1 = open(args[1], O_APPEND);
 			int fd2 = open(args[2], O_APPEND);
 			if (fd1 < 0 || fd2 < 0)
 			{
 				perror("smash error"); //  bad path/s
-				return 1;
 			}
 
 			else // good paths
@@ -176,6 +245,7 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 				char text1[CHUNK];
 				int bytes1 = 1;
 				int bytes2 = 1;
+				int res = 0;
 
 				while (bytes1 > 0 && bytes2 > 0)
 				{
@@ -184,18 +254,22 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 					if (bytes1 < 0 || bytes2 < 0)
 					{
 						perror("smash error"); //  bad reads
-						return 1;
+						res = -1;
+						break;
 					}
 
 					if ((string)text1 != (string)text2)
 					{
-						cout << "1" << endl;
-						return 0;
+						res = 1;
+						break;
 					}
 
 				}
 
-				cout << "0" << endl;
+				if (res >= 0)
+				{
+					cout << res << endl;
+				}
 			}
 		}
 
@@ -214,41 +288,60 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 			if (src_fd < 0)
 			{
 				perror("smash error"); // src file does not exist
-				return 1;
 			}
 
 			int dst_fd = creat(args[2], S_IRWXU | S_IRWXG);
-			if (dst_fd < 0)
+			else if (dst_fd < 0)
 			{
-				perror("smash error"); // dst cannot be opened
-				return 1;
+				perror("smash error"); // dst file does not exist/cannot be opened
 			}
-
-			char src_text[CHUNK];
-			int rd_bytes = 1;
-			int wr_bytes;
-
-			while (rd_bytes > 0)
+			
+			else
 			{
-				rd_bytes = read(src_fd, src_text, CHUNK);
-				if (rd_bytes < 0)
-				{
-					perror("smash error"); //  bad read
-					return 1;
-				}
 
-				wr_bytes = write(dst_fd, src_text, rd_bytes);
-				if (wr_bytes < 0 || wr_bytes != rd_bytes)
+				char src_text[CHUNK];
+				int rd_bytes = 1;
+				int wr_bytes;
+
+				while (rd_bytes > 0)
 				{
-					perror("smash error"); // bad write
-					return 1;
+					rd_bytes = read(src_fd, src_text, CHUNK);
+					if (rd_bytes < 0)
+					{
+						perror("smash error"); //  bad read
+						break;
+					}
+
+					wr_bytes = write(dst_fd, src_text, rd_bytes);
+					if (wr_bytes < 0 || wr_bytes != rd_bytes)
+					{
+						perror("smash error"); // bad write
+						break;
+					}
 				}
 			}
-
-			return 0;
 		}
 	}
 	/*************************************************/
+	else if (!strcmp(cmd, "kill"))
+	{
+		if (num_arg != 3)
+		{
+			illegal_cmd = TRUE;
+		}
+		int job_pid = get_job_pid(jobs, args[2]);
+		if (job_pid == -1)
+		{
+			cout << "smash error: > kill " << args[2] << "– job does not exist" << endl;
+		}
+		int signum = -1 * (int)args[1];
+		if (kill(job_pid, signum) != 0)
+		{
+			cout << "smash error: > kill " << args[2] << "– cannot send signal" << endl;
+		}
+	}
+	/*************************************************/
+
 	else // external command
 	{
  		ExeExternal(args, cmdString);
@@ -287,9 +380,7 @@ void ExeExternal(char *args[MAX_ARG], char* cmdString)
 			default:
                 	// Add your code here
 					
-					/* 
-					your code
-					*/
+					waitpid(pID, NULL, 0);
 	}
 }
 //**************************************************************************************
@@ -326,13 +417,30 @@ int BgCmd(char* lineSize, void* jobs)
 	char *args[MAX_ARG];
 	if (lineSize[strlen(lineSize)-2] == '&')
 	{
-		lineSize[strlen(lineSize)-2] = '\0';
+		lineSize[strlen(lineSize)-2] = NUL;
 		// Add your code here (execute a in the background)
-					
-		/* 
-		your code
-		*/
 		
+		char* cmd;
+		int i = 0, num_arg = 0;
+		cmd = strtok(lineSize, delimiters);
+		if (cmd == NULL)
+			return 0;
+		args[0] = cmd;
+		for (i = 1; i < MAX_ARG; i++)
+		{
+			args[i] = strtok(NULL, delimiters);
+			if (args[i] != NULL)
+				num_arg++;
+
+		}
+
+		pid_t cpid = fork();
+
+		if (cpid == 0)	// child proc to run in bg
+		{
+
+		}
+		return 0;
 	}
 	return -1;
 }
